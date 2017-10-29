966788121335bd8f18bb694e2322909a392764a1
Arne Janssens <arne.janssens@barco.com>
2017-10-29 11:11:23 +0100

    unit testing

--- myapp/src/main.cpp
+++ myapp/src/main.cpp
@@ -1,8 +1,16 @@
 #include <mylibrary/mylibrary.h>
 
-int main()
+#include <iostream>
+
+int main(int const argc, char const *argv[])
 {
-	print_funny("Hello World !");
+	if(argc != 2)
+	{
+		std::cerr << "Wrong number of arguments" << std::endl;
+		return 1;
+	}
+	
+	print_funny(argv[1]);
 
 	return 0;
 }
